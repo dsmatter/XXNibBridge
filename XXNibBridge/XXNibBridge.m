42b732eeb9f1eca92a79951ddb29bf314b29db9d
Daniel Strittmatter <daniel@smattr.de>
2016-09-18 12:21:07 +0200

    Execute constraint copying asynchronously
    This fixes crashes where the replaced constraint is sill retained by a trait
    collection but references a deallocated firstItem.
    
    See https://github.com/sunnyxx/XXNibBridge/issues/29

--- XXNibBridge/XXNibBridge.m
+++ XXNibBridge/XXNibBridge.m
@@ -70,50 +70,50 @@
     
     // Copy autolayout constrains.
     if (placeholderView.constraints.count > 0) {
-        
-        // We only need to copy "self" constraints (like width/height constraints)
-        // from placeholder to real view
-        for (NSLayoutConstraint *constraint in placeholderView.constraints) {
-            
-            NSLayoutConstraint* newConstraint;
-            
-            // "Height" or "Width" constraint
-            // "self" as its first item, no second item
-            if (!constraint.secondItem) {
-                newConstraint =
-                [NSLayoutConstraint constraintWithItem:realView
-                                             attribute:constraint.firstAttribute
-                                             relatedBy:constraint.relation
-                                                toItem:nil
-                                             attribute:constraint.secondAttribute
-                                            multiplier:constraint.multiplier
-                                              constant:constraint.constant];
-            }
-            // "Aspect ratio" constraint
-            // "self" as its first AND second item
-            else if ([constraint.firstItem isEqual:constraint.secondItem]) {
-                newConstraint =
-                [NSLayoutConstraint constraintWithItem:realView
-                                             attribute:constraint.firstAttribute
-                                             relatedBy:constraint.relation
-                                                toItem:realView
-                                             attribute:constraint.secondAttribute
-                                            multiplier:constraint.multiplier
-                                              constant:constraint.constant];
-            }
-            
-            // Copy properties to new constraint
-            if (newConstraint) {
-                newConstraint.shouldBeArchived = constraint.shouldBeArchived;
-                newConstraint.priority = constraint.priority;
-                if ([UIDevice currentDevice].systemVersion.floatValue >= 7.0f) {
-                    newConstraint.identifier = constraint.identifier;
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+            // We only need to copy "self" constraints (like width/height constraints)
+            // from placeholder to real view
+            for (NSLayoutConstraint *constraint in placeholderView.constraints) {
+                NSLayoutConstraint* newConstraint;
+
+                // "Height" or "Width" constraint
+                // "self" as its first item, no second item
+                if (!constraint.secondItem) {
+                    newConstraint =
+                    [NSLayoutConstraint constraintWithItem:realView
+                                                 attribute:constraint.firstAttribute
+                                                 relatedBy:constraint.relation
+                                                    toItem:nil
+                                                 attribute:constraint.secondAttribute
+                                                multiplier:constraint.multiplier
+                                                  constant:constraint.constant];
                 }
-                [realView addConstraint:newConstraint];
+                // "Aspect ratio" constraint
+                // "self" as its first AND second item
+                else if ([constraint.firstItem isEqual:constraint.secondItem]) {
+                    newConstraint =
+                    [NSLayoutConstraint constraintWithItem:realView
+                                                 attribute:constraint.firstAttribute
+                                                 relatedBy:constraint.relation
+                                                    toItem:realView
+                                                 attribute:constraint.secondAttribute
+                                                multiplier:constraint.multiplier
+                                                  constant:constraint.constant];
+                }
+
+                // Copy properties to new constraint
+                if (newConstraint) {
+                    newConstraint.shouldBeArchived = constraint.shouldBeArchived;
+                    newConstraint.priority = constraint.priority;
+                    if ([UIDevice currentDevice].systemVersion.floatValue >= 7.0f) {
+                        newConstraint.identifier = constraint.identifier;
+                    }
+                    [realView addConstraint:newConstraint];
+                }
             }
-        }
+        });
     }
-    
+  
     return realView;
 }
 
